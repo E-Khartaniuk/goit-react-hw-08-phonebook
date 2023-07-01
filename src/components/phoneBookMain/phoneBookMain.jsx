2a00a3c83cc855aa853f95379c9c07a3d61dc2d6
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-07-02 01:21:33 +0300

    8

--- src/components/phoneBookMain/phoneBookMain.jsx
+++ src/components/phoneBookMain/phoneBookMain.jsx
@@ -1,7 +1,6 @@
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import { PhoneBookForm } from 'components/phonebook/PhoneBookForm';
-import { Routes } from 'react-router-dom';
 
 export default function PhoneBookMain() {
   return (
