b8dd30dc81d61281f92bc22531fbe4df6b614ad3
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-07-02 10:14:19 +0300

    8

--- src/components/phoneBookMain/phoneBookMain.jsx
+++ src/components/phoneBookMain/phoneBookMain.jsx
@@ -2,7 +2,7 @@ import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import PhoneBookForm from 'components/Phonebook/PhoneBookForm';
 
-export default function PhoneBookMain() {
+function PhoneBookMain() {
   return (
     <>
       <PhoneBookForm />
@@ -11,3 +11,4 @@ export default function PhoneBookMain() {
     </>
   );
 }
+export default PhoneBookMain;
