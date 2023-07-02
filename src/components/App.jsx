b3ae0e91bc933c7d4ed5b4e1ad8f4e591e1f632b
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-07-02 11:39:21 +0300

    8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import { Route, Routes } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
+
 import { Navigation } from './Navigation/Navigation';
 
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import PublicRoute from './PublicRoute/PublicRoute';
-import UserMenu from '../pages/UserMenu';
-import SignUpPage from '../pages/SignUp';
-import PhoneBookContainer from '../pages/PhoneBookContainer';
-import LogInPage from '../pages/LogInPage';
+import UserMenu from './UserMenu/UserMenu';
+import LogInPage from 'pages/LogInPage';
+import SignUpPage from 'pages/SignUp';
+import PhoneBookMain from 'pages/PhoneBookMain';
+import HomePage from 'pages/HomePage';
 
 export function App() {
   return (
@@ -17,74 +18,39 @@ export function App() {
       <Route path="/" element={<Navigation />}>
         <Route index element={<HomePage />} />
         <Route path="usermenu" element={<UserMenu />}></Route>
-      </Route>
-
-      <Route
-        path="contacts"
-        element={
-          <PrivateRoute>
-            <PhoneBookContainer />
-          </PrivateRoute>
-        }
-      ></Route>
-      <Route
-        path="register"
-        element={
-          <PublicRoute>
-            <SignUpPage />
-          </PublicRoute>
-        }
-      ></Route>
-      <Route
-        path="login"
-        element={
-          <PublicRoute>
-            <LogInPage />
-          </PublicRoute>
-        }
-      ></Route>
-      <Route
-        path="/usermenu"
-        element={
-          <PrivateRoute>
-            <UserMenu />
-          </PrivateRoute>
-        }
-      ></Route>
-
-      {/* <Route
-        path="contacts"
-        element={
-          <PrivateRoute>
-            <PhoneBookContainer />
-          </PrivateRoute>
-        }
-      > */}
-      {/* <Route
+        <Route
+          path="contacts"
+          element={
+            <PrivateRoute>
+              <PhoneBookMain />
+            </PrivateRoute>
+          }
+        />
+        <Route
           path="register"
           element={
             <PublicRoute>
               <SignUpPage />
             </PublicRoute>
           }
-        ></Route>{' '} */}
-      {/* <Route
+        />
+        <Route
           path="login"
           element={
             <PublicRoute>
               <LogInPage />
             </PublicRoute>
           }
-        ></Route> */}
-      {/* </Route>
+        />
+      </Route>
       <Route
         path="/usermenu"
         element={
           <PrivateRoute>
             <UserMenu />
           </PrivateRoute>
-        } */}
-      {/* /> */}
+        }
+      />
 
       <Route path="*" element={<Navigation />} />
     </Routes>
