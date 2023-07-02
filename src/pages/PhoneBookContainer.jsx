d2e58fd95eae23797b4ed46375c13436626bb928
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-07-02 11:45:19 +0300

    8

--- src/pages/PhoneBookContainer.jsx
+++ src/pages/PhoneBookContainer.jsx
@@ -1,6 +1,6 @@
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
-import PhoneBookForm from 'components/Phonebook/PhoneBookForm.jsx';
+import PhoneBookForm from '../components/Phonebook/PhoneBookForm';
 
 export default function PhoneBookContainer() {
   return (
